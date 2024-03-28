e9d034d5f137bfd074b7914638474996ef4de2c3
IgorKondratowicz <igorkondratowicz2003@gmail.com>
2024-03-28 20:25:36 +0100

    cleanup

--- src/js/cms/pages/files.js
+++ src/js/cms/pages/files.js
@@ -4,7 +4,6 @@ const csrfToken = document.querySelector('meta[name="csrf"]').content;
 const template = document.querySelector('meta[name="template"]').content;
 
 if (template === 'cms_files') {
-    console.log('files')
     new FileManager({
         token: csrfToken,
         target: 'main div.files_main_container'
