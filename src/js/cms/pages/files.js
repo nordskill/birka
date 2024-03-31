0a9e85771933bfc2fa77f3f1d12c94ee5471c78a
Oleksii <26419775+nordskill@users.noreply.github.com>
2024-04-01 00:51:10 +0200

    fix: page module didn't export any function

--- src/js/cms/pages/files.js
+++ src/js/cms/pages/files.js
@@ -3,9 +3,13 @@ import FileManager from '../../components/file-manager';
 const csrfToken = document.querySelector('meta[name="csrf"]').content;
 const template = document.querySelector('meta[name="template"]').content;
 
-if (template === 'cms_files') {
-    new FileManager({
-        token: csrfToken,
-        target: 'main div.files_main_container'
-    });
+export default function() {
+
+    if (template === 'cms_files') {
+        new FileManager({
+            token: csrfToken,
+            target: 'main div.files_main_container'
+        });
+    }
+
 }
