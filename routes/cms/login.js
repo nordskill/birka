d6a5438656b919dc8956ccbdc06ff65b26b89efa
Oleksii <26419775+nordskill@users.noreply.github.com>
2024-04-05 18:37:54 +0200

    env: function docs

--- routes/cms/login.js
+++ routes/cms/login.js
@@ -25,6 +25,12 @@ router.get('/', async (req, res, next) => {
 
 module.exports = router;
 
+
+/**
+ * Find the number (1st param) in an array (2nd param) that is closest to a given number.
+ * @example
+ * findClosestNumber(5, [1, 3, 6, 8]); // 6
+ */
 function findClosestNumber(target, numbers) {
     return numbers.reduce((closest, num) => Math.abs(num - target) < Math.abs(closest - target) ? num : closest);
 }
