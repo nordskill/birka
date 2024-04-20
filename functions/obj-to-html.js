af934530046de319c7a41e84ed9f9e2cdd814ee3
Oleksii <26419775+nordskill@users.noreply.github.com>
2024-04-21 01:57:59 +0200

    fix: attributes not found in body

--- functions/obj-to-html.js
+++ functions/obj-to-html.js
@@ -4,22 +4,22 @@ function OBJtoHTML(body) {
 	body.forEach((element) => {
 		switch (element.type) {
 			case 'paragraph':
-				const textAlign = element.attributes.align ? `style="text-align: ${element.attributes.align}"` : '';
+				const textAlign = element.attributes?.align ? `style="text-align: ${element.attributes.align}"` : '';
 				html += `<p ${textAlign}>${element.content}</p>\r\n`;
 				break;
 			case 'heading':
-				html += `<h${element.attributes.level}>${element.content}</h${element.attributes.level}>\r\n`;
+				html += `<h${element.attributes?.level}>${element.content}</h${element.attributes.level}>\r\n`;
 				break;
 			case 'list':
 				const listHtml = element.content.map(item => `\r\n\t<li>${item.item}</li>`).join('');
-				if (element.attributes.type === 'unordered') {
+				if (element.attributes?.type === 'unordered') {
 					html += `<ul>${listHtml}</ul>\r\n`;
 				} else {
 					html += `<ol>${listHtml}</ol>\r\n`;
 				}
 				break;
 			case 'image':
-				const classAttr = element.attributes.width ? `class="${element.attributes.width}"` : '';
+				const classAttr = element.attributes?.width ? `class="${element.attributes.width}"` : '';
 				html += `<picture ${classAttr}>\r\n\t<img src="${element.content}" alt="${element.attributes.alt}">\r\n\t<figcaption>${element.attributes.alt}</figcaption>\r\n</picture>\r\n`;
 				break;
 			case 'quote':
