83a51ad504bd5435629266355bce47b3a2acdfc3
Oleksii <26419775+nordskill@users.noreply.github.com>
2024-06-08 22:22:19 +0200

    update: optimized page data

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ const OperationalError = require('../functions/operational-error');
 router.get('/:slug', async (req, res, next) => {
     try {
         const slug = req.params.slug;
-        const page = await Page.findOne({ slug: slug });
+        const page = await Page.findOne({ slug: slug }).lean();
         if (page) {
             res.render(page.template, {
                 title: page.name,
-                excerpt: page.excerpt,
-                template_name: page.template
+                template_name: page.template,
+                page
             });
         } else {
             throw new OperationalError('Page not found', 404);
@@ -25,19 +25,20 @@ router.get('/:slug', async (req, res, next) => {
 // Route for fetching the homepage
 router.get('/', async (req, res, next) => {
     try {
-        const page = await Page.findOne({ is_home: true });
+        const page = await Page.findOne({ is_home: true }).lean();
         if (page) {
             res.render(page.template, {
                 title: page.name,
-                excerpt: page.excerpt,
-                template_name: page.template
+                template_name: page.template,
+                page
             });
         } else { // if no home page is set, render a default template
             const defaultTemplate = 'default_home';
             res.render(defaultTemplate, {
                 title: 'This is a Default Home Page',
                 content: 'Because none of the pages are marked as home.',
-                template_name: defaultTemplate
+                template_name: defaultTemplate,
+                page
             });
         }
     } catch (error) {
