1bb2c745afb8d7d13dff601ab474a1393323ab09
Oleksii <26419775+nordskill@users.noreply.github.com>
2024-06-27 17:37:35 +0200

    update: handle dot-notated field path updates

--- core/routes/api/pages.js
+++ core/routes/api/pages.js
@@ -47,37 +47,46 @@ router.post('/', async (req, res, next) => {
 });
 
 router.patch('/:id', async (req, res, next) => {
-
+    
     const id = req.params.id;
     const { unset, ...updates } = req.body;
-    const updateOperation = { ...updates };
-    const type = updateOperation.type;
+    const type = updates.type; // Assuming 'type' is used for model discrimination
+
+    // Initialize update operations
+    const updateOperation = { $set: {}, $unset: {} };
 
     try {
-
+        // Validate ID
         if (!mongoose.isValidObjectId(id)) {
             throw new OperationalError("Invalid ID format", 400);
         }
 
+        // Handle unsetting fields
         if (unset) {
-            updateOperation.$unset = {};
             unset.forEach(field => {
                 updateOperation.$unset[field] = "";
-            })
+            });
         }
 
-        // Find model based on discriminator 'type'
+        // Handle setting new values
+        for (const [key, value] of Object.entries(updates)) {
+            if (key !== 'type') { // Exclude 'type' from being directly set in the update operation
+                updateOperation.$set[key] = value;
+            }
+        }
+
+        // Find the correct model based on discriminator 'type'
         const PageModel = type ? Page.discriminators[type] : Page;
         if (!PageModel) {
             throw new OperationalError(`Invalid page type: ${type}`, 400);
         }
 
-        // Check if updateOperation requests is_home to be true
-        if (updateOperation.is_home === true) {
-            // Set is_home to false for all other pages
+        // Check if updateOperation requests is_home to be true and update accordingly
+        if (updateOperation.$set.is_home === true) {
             await Page.updateMany({ _id: { $ne: id }, is_home: true }, { $set: { is_home: false } });
         }
 
+        // Execute the update with options for returning the new document and running validators
         const options = { new: true, runValidators: true };
         const updatedPage = await PageModel.findByIdAndUpdate(id, updateOperation, options);
 
@@ -94,6 +103,7 @@ router.patch('/:id', async (req, res, next) => {
         next(error);
     }
 });
+
 
 router.delete('/:id', async (req, res, next) => {
     try {
